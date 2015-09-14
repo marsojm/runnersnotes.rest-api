6ddb8f21ade3d9b18ab8979a9d74aa1bb36ad43c
Marko Malinen <markojm1986@gmail.com>
2015-09-14 18:28:39 +0300

    Remove alerts when clicking link or button

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -12,23 +12,25 @@
             	rn.notes = data.items;
         });
 
+        this.clearAlerts = function() {
+            rn.errors = [];
+            rn.success = false;
+        };
+
         this.addNote = function() {
             rn.note.created = new Date(rn.note.date).getTime();
             $http.post('http://localhost:8080/notes',rn.note).
                 then(function(response){
-                    //if (response.status === 201) {
-                        rn.notes.push(rn.note);
-                        rn.note = {};
-                        rn.success = true;
-                        rn.errors = [];
-                        $("#addForm").collapse('hide');
-                    //}
+                    rn.notes.push(rn.note);
+                    rn.note = {};
+                    rn.success = true;
+                    rn.errors = [];
+                    $("#addForm").collapse('hide');
                 }, function(response) {
                     rn.success = false;
                     for (var i=0; i < response.data.length; i++) {
                         rn.errors.push(response.data[i]);
                     }
-                    console.log(response);
                 });
         };
     }]);
